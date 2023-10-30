cc7b1f1896e053558ca3bff129bbdc44e829364a
Wilfor Ibanez <aln.wil4@outlook.es>
2023-10-30 15:49:42 -0400

    Config file added
    Configuration class to extract the principal data instead of having it on the main test code

---
+++ src/test/java/config/Configuration.java
@@ -0,0 +1,10 @@
+package config;
+
+public class Configuration {
+    public static String host = "https://todo.ly";
+    public static String email = "user@testing.com";
+    public static String fullName = "userTesting";
+    public static String password = "12345";
+
+    public static String updatedEmail = "userqatest@testing.com";
+}
